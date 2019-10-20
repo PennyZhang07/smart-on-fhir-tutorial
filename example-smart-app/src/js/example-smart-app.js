53d7bd073634e8fdabd0e95d1d70e9e2f3b23688
PennyZhang07 <zhangpei1128@gmail.com>
2019-10-20 11:47:10 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,3 +1,24 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@PennyZhang07 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+24
+621.9kcerner/smart-on-fhir-tutorial
+ Code Issues 2 Pull requests 1 Actions Projects 0 Security Insights
+smart-on-fhir-tutorial/example-smart-app/src/js/example-smart-app.js
+@kpshek kpshek publish: Merge pull request #55 from cerner/uplift-dependencies
+fdcd5ff on Jun 5, 2018
+132 lines (111 sloc)  3.73 KB
+  
 (function(window){
   window.extractData = function() {
     var ret = $.Deferred();
@@ -17,8 +38,7 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4'
-                             'http://loinc.org|29463-7']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
                     }
                   });
@@ -38,7 +58,6 @@
           }
 
           var height = byCodes('8302-2');
-          var weight = byCodes ('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -49,9 +68,7 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0])
-          p.weight = getQuantityValueAndUnit(weight[0])
-          ;
+          p.height = getQuantityValueAndUnit(height[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -83,7 +100,6 @@
       gender: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
-      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
@@ -127,7 +143,6 @@
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
-    $('#weight').html(p.weight);  
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
@@ -135,3 +150,15 @@
   };
 
 })(window);
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
